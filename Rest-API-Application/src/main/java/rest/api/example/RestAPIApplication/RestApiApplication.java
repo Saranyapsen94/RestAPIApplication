84122d9ed73192815ab9d15744fbbc547830e869
saranya <saranyapsen94@gmail.com>
2024-05-07 12:55:55 -0400

    DB connection changes

--- Rest-API-Application/src/main/java/rest/api/example/RestAPIApplication/RestApiApplication.java
+++ Rest-API-Application/src/main/java/rest/api/example/RestAPIApplication/RestApiApplication.java
@@ -1,13 +1,32 @@
 package rest.api.example.RestAPIApplication;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+
+import rest.api.example.Entity.RestApiEntity;
 
 @SpringBootApplication
-public class RestApiApplication {
+public class RestApiApplication implements CommandLineRunner{
 
+	@Autowired 
+	JdbcTemplate jdbcTemplate;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(RestApiApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		String sql = "SELECT * from department";
+		List<RestApiEntity> lst = jdbcTemplate.query(sql, BeanPropertyRowMapper.newInstance(RestApiEntity.class));
+			lst.forEach(System.out::println);	
 	}
 
 }

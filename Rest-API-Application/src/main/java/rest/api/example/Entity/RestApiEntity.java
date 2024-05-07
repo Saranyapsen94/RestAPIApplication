0d8735dd0253d70314ce3b487dd0b6df0236d0da
saranya <saranyapsen94@gmail.com>
2024-05-07 12:56:45 -0400

    Entity changes

---
+++ Rest-API-Application/src/main/java/rest/api/example/Entity/RestApiEntity.java
@@ -0,0 +1,29 @@
+package rest.api.example.Entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
+public class RestApiEntity {
+
+	@Id
+	private int id;
+	private String name;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	@Override
+	public String toString() {
+		return "RestApiEntity [id=" + id + ", name=" + name + "]";
+	}
+	
+}
